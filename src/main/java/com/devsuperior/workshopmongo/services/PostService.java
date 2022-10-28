b2dbf9f6e7311ed71df51baed98b4ce92b1b8c26
wcruzx <092.1307@alunos.unigrancapital.com.br>
2022-10-27 21:15:21 -0400

    Getting a post by id

---
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.workshopmongo.domain.Post;
+import com.devsuperior.workshopmongo.repository.PostRepository;
+import com.devsuperior.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
