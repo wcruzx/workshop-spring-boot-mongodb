c28896e93f7d36935a1eb17e3a7f699de73251b6
wcruzx <092.1307@alunos.unigrancapital.com.br>
2022-10-27 21:27:48 -0400

    Adding comments to posts

--- src/main/java/com/devsuperior/workshopmongo/domain/Post.java
+++ src/main/java/com/devsuperior/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.devsuperior.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.devsuperior.workshopmongo.dto.AuthorDTO;
+import com.devsuperior.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
 	}
 
@@ -29,6 +34,14 @@ public class Post implements Serializable {
 		this.title = title;
 		this.body = body;
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	public String getId() {
