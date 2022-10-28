230e503a5fec77aafc4a5d9d35f5ffe87292a4bb
wcruzx <092.1307@alunos.unigrancapital.com.br>
2022-10-28 00:28:21 -0400

    Search with multiple parameters

--- src/main/java/com/devsuperior/workshopmongo/resources/util/URL.java
+++ src/main/java/com/devsuperior/workshopmongo/resources/util/URL.java
@@ -2,6 +2,10 @@ package com.devsuperior.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -11,5 +15,15 @@ public class URL {
 		} catch (UnsupportedEncodingException e) {
 			return "";
 		}
+	}
+
+	public static java.util.Date convertDate(String textDate, java.util.Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+				return (Date) sdf.parse(textDate);
+		} catch (ParseException e) {
+				return date;
+		}		
 	}
 }
--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.workshopmongo.services;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,10 @@ public class PostService {
 
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
